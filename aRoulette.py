7301b8682bf1fffee861667fc38c5d544b1609f0
Jared Farrar <90429418+Kangaroojack14@users.noreply.github.com>
2026-04-29 21:18:30 -0700

    add: add american roulette game

---
+++ aRoulette.py
@@ -0,0 +1,20 @@
+import random as rand
+#Class for a game of American Roulette
+class A_Roulette(Gambler):
+
+    def __init__(self, bets = []):
+        #37 represents 00
+        self.tableNums = list(range[0, 37])
+
+    #calculate the pocket the marble lands on
+    def Marble(self):
+        winNum = rand.randrange(37)
+        return winNum
+    
+    def Play(self, bets, betAmount):
+        win = Marble()
+        #check to see if any bets hit, calculate and return money lost or gained
+        for x in bets:
+            if x == win:
+                return (betAmount * (36/len(bets) - 1))
+        return -betAmount
